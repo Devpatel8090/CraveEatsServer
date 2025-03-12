36c9b7c5fd1272b191131a796a1f9031cb219a43
Dev vipulkumar patel <83284706+Devpatel8090@users.noreply.github.com>
2025-03-12 12:54:14 -0700

    completed overview page with fully backend integration and dynamic data

--- API/Food/index.js
+++ API/Food/index.js
@@ -9,6 +9,25 @@ import { FoodModel } from "../../database/allModelsIndex";
 import { ValidateCategory, ValidateId } from "../../Validation/common.validation";
 const Router = express.Router();
 
+
+/**
+ * Route        /:_id
+ * Des          GET food based on id
+ * Params       _id
+ * Access       Public
+ * Method       GET
+ */
+Router.get("/:_id", async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const foods = await FoodModel.findById(_id);
+        return res.json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+
 /**
  * Route           /r/:_id  
  * Des              Get all food based on particular restaurants
