2a85360f9db623f24589720b52cfc4267584b7da
Dev vipulkumar patel <83284706+Devpatel8090@users.noreply.github.com>
2025-06-17 22:42:29 -0700

    Remove existing server folder to link subtree

--- server/build/database/Type/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var TypeSchema = mongoose.Schema({});
--- server/src/database/Type/index.js
+++
@@ -1,8 +0,0 @@
-import mangoose from "mongoose";
-
-const TypeSchema = mongoose.Schema(
-    {
-
-    }
-
-)
